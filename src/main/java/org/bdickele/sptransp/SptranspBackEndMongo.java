b79c147482119939de46c904863d2a625f40b559
Bertrand DICKELE <bdickele@gmail.com>
2015-12-04 22:56:09 +0100

    First commit

---
+++ src/main/java/org/bdickele/sptransp/SptranspBackEndMongo.java
@@ -0,0 +1,12 @@
+package org.bdickele.sptransp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptranspBackEndMongo {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SptranspBackEndMongo.class, args);
+    }
+}
